36d03613d7390dced046068c06f873026661acbc
anthonymjimenez <anthonyjim96@gmail.com>
2020-10-12 13:19:56 -0400

    submit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,15 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new
+    @student = Student.new
+  end
+
+  def create 
+    @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+    @student.save
+    redirect_to students_path
+  end
 
 end
